a4280379bba99f82388cca7ad163f184440e35d9
vcyreg <vcyreg@gmail.com>
2020-05-11 03:55:52 -0400

    connections list

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,8 +1,9 @@
 import React from 'react'
 
 export const Connection = ({ data }) => (
-  <article className="data-excerpt">
-    <h2>{data.Name}</h2>
-    <p>{data.Type}</p>
-  </article>
+    <>
+        <td>{data.Name}</td>
+        <td>{data.Type}</td>
+        <td>{data.DbName}</td>
+    </>
 )
--- src/pages/ConnectionsPage.js
+++ src/pages/ConnectionsPage.js
@@ -5,28 +5,40 @@ import { Connection } from '../components/Connection'
 import { fetchConnections } from '../actions/connectionsAction'
 
 const ConnectsPage = ({ dispatch, loading, connections, hasErrors }) => {
-  useEffect(() => {
-    dispatch(fetchConnections())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(fetchConnections())
+    }, [dispatch])
 
-  const renderConnections = () => {
-    if (loading) return <p>Loading connections...</p>
-    if (hasErrors) return <p>Unable to display connections list.</p>
-    return connections.map(connection => <Connection key={connection.id} data={connection} />)
-  }
+    const renderConnections = () => {
+        if (loading) return <p>Loading connections...</p>
+        if (hasErrors) return <p>Unable to display connections list.</p>
+        return (
+            <table className="table table-hover">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Type</th>
+                        <th>Data Base</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        {connections.map(connection => <Connection key={connection.id} data={connection} />)}
+                    </tr>
+                </tbody>
+            </table>)
+    }
+    
 
-  return (
-    <section>
-      <h1>connect list</h1>
-      {renderConnections()}
-    </section>
-  )
+    return (<div>{renderConnections()}</div>)
 }
 
+
 const mapStateToProps = state => ({
-  loading: state.connections.loading,
-  connections: state.connections.connections,
-  hasErrors: state.connections.hasErrors,
+    loading: state.connections.loading,
+    connections: state.connections.connections,
+    hasErrors: state.connections.hasErrors,
 })
 
 export default connect(mapStateToProps)(ConnectsPage)
